ead0835ade5b9f13cf4fb7ac66c5cc7c489bb406
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-02-19 15:46:01 -0500

    Adds functions.

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(l){
+  if (l.length > 20) {
+    return "That's a long string!";
+  }
+}
+
+function isItMedium(m){
+  if (m.length >= 10 && m.length <= 20){
+    return "That's a regular sized string!";
+  }
+}
+
+function isItShort(s){
+  if (s.length < 10){
+    return "That's a small string!";
+  } else{
+    return "That's not a small string!"
+  }
+}
+
+function howLongIsMyString(s){
+  if (s.length > 20){
+    return "That's a long string!"
+  } else if (s.length >= 10 && s.length <= 20){
+    return "That's a regular sized string!"
+  } else {
+    return "That's a small string!"
+  }
+}
+
+function instructorHeight(n){
+  if (n === 'Colin'){
+    return 62;
+  } else if (n === 'Mesuara'){
+    return 69;
+  } else{
+    return "I don't know that instructor!"
+  }
+}
+
 
 
 
